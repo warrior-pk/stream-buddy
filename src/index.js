643fbbeef7dae6d3e97ddc7247130d9329098d8e
Piyush Kumar <piyu8h@gmail.com>
2024-02-26 01:44:09 +0530

    Implemented custom Api Error, Response, Async Wrapper utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error(`MongoDB connection failed ${e}`);
+  });
