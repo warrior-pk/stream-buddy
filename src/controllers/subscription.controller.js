c75b808d36a7fd0601120542dda7f4163994a4f9
Piyush Kumar <piyu8h@gmail.com>
2024-02-29 02:02:39 +0530

    subs: get subscription done

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -41,12 +41,70 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     );
 });
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  if (!isValidObjectId(channelId.trim())) {
+    throw new ApiError(400, "Invalid channelId");
+  }
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId.trim()),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "user",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+              coverImage: 1,
+              subscribersCount: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        user: {
+          $first: "$user",
+        },
+      },
+    },
+  ]);
+
+  if (!subscribers) {
+    throw new ApiError(500, "Couldn't fetch channels");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, subscribers, "Channels fetched successfully"));
 });
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
   if (!isValidObjectId(subscriberId.trim())) {
